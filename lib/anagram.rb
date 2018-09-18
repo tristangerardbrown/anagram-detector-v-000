2cd11d48348eb55e92d1dd3d3fdd0e184abd1e35
Tristan Brown <tgbrown@stanford.edu>
2018-09-18 19:22:24 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,14 +8,13 @@ class Anagram
   def match(word_array)
     anagrams = []
 
-    sorted_word = @word.split("").sort
-
     word_array.each do |word|
-      word2= word.split("").sort
-      if word2 == sorted_word
+      word2 = word.split("").sort
+      if word2 == @word.split("").sort
         anagrams << word
       end
     end
+    anagrams 
   end
 
   end
